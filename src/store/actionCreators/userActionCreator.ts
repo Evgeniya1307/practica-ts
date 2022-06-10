8fbce750caf0e7fb741b4d4da0a00133ab348434
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-06-10 13:23:51 +0400

    dispatch({type: UserActionTypes.FETCH_USERS}) // в диспатч передаю action FETCH_USERS который londing сделает true и на сайте повятся крутилка

--- src/store/actionCreators/userActionCreator.ts
+++ src/store/actionCreators/userActionCreator.ts
@@ -4,15 +4,15 @@ import { UserActions, UserActionTypes } from "../../types/todo"
 
 // находятся экшенкрейтары которые связаны конткретно с пользователями
 export const fetchUsers = ():any => async  (dispatch: Dispatch<UserActions>) => { //параметром принимает диспатч и внутри делаю запрос на сервер 
-    try {
-        dispatch({type: UserActionTypes.FETCH_USERS})
+    try { // отлавливать ошибки
+        dispatch({type: UserActionTypes.FETCH_USERS}) // в диспатч передаю action FETCH_USERS который londing сделает true и на сайте повятся крутилка
         const response = await axios.get('https://jsonplaceholder.typicode.com/users')
 
         setTimeout(() => {
             dispatch({type: UserActionTypes.FETCH_USERS_SUCCESS, payload: response.data})
         }, 500)
 
-    } catch (error) {
+    } catch (error) { // если возникла ошибка я передала FETCH_USERS_ERROR
         dispatch({
             type: UserActionTypes.FETCH_USERS_ERROR, 
             payload: 'Request was be with error'})
